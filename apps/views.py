eff3aa11ec6f59430db8be50245e79c0f60efa43
gurulakshmikanth <gurukanth121.com>
2023-11-09 19:04:55 +0530

    first commit

---
+++ apps/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mawa(request):
+    return render(request,'mawa.html')
+def bro(request):
+    return render(request,'bro.html')
+def macha(request):
+    return render(request,'macha.html')
+def bava(request):
+    return render(request,'bava.html')
+
+
+
